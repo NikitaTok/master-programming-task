857fda969c6748d1536fd876e123195eab7a4444
NikitaTok <nek.tokar20466@mail.ru>
2024-12-01 09:11:15 +0000

    lab4 и lab5

---
+++ lab4/CMakeFiles/tests_iterator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_iterator.dir/tests/iterator.cpp.o"
+  "tests_iterator"
+  "tests_iterator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_iterator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab4/include/iterator.hpp
+++ lab4/include/iterator.hpp
@@ -1,62 +1,59 @@
+/* Image iterator.
+ * @file
+ * @date 2018-08-05
+ * @author Anonymous
+ */
+
 #ifndef __ITERATOR_HPP__
 #define __ITERATOR_HPP__
+
 #include <cmath>
 #include <iterator>
 #include <type_traits>
 #include <utility>
-#include </boost/iterator/iterator_adaptor.hpp>
-#include <type_traits>
-#include <vector>
-#include <algorithm>
 
-//{ image iterator
+#include <boost/iterator/iterator_adaptor.hpp>
+
+ //{ image iterator
 template<class Iterator>
+class image_iterator : public boost::iterator_adaptor<
+    image_iterator<Iterator>,
+    Iterator
+>
+{
+public:
+    image_iterator(Iterator it, size_t width, size_t stride) 
+    : boost::iterator_adaptor<image_iterator<Iterator>, Iterator>(it), width(width), stride(stride), index(0)
+    {}
 
-class image_iterator: public boost::iterator_adaptor<image_iterator<Iterator>, Iterator> {
-    using AdaptorType = boost::iterator_adaptor<image_iterator<Iterator>, Iterator>;
-    
-    public:
-    image_iterator(Iterator input_iterator, size_t width_id, size_t stride_t) :
-    
-    AdaptorType(input_iterator), stride(stride_t), width(width_id) {}
-    
-    private:
+private:
     friend class boost::iterator_core_access;
-    
-    typename AdaptorType::reference dereference() const {
-        return *(this->base_reference());
-        }
-        
-    void increment() {
+    size_t width;
+    size_t stride;
+    int index;
+
+    void increment()
+    {
         advance(1);
-        }
-        
-    void decrement() {
+    }
+
+    void decrement()
+    {
         advance(-1);
     }
-    
-    void advance(typename AdaptorType::difference_type d_t_p) {
-        ptrdiff_t count_rows = std::floor((crnt_pos + d_t_p) / double(width));
-        crnt_pos += d_t_p;
-        crnt_pos %= width;
-        this->base_reference() = this->base_reference() + d_t_p + count_rows * ptrdiff_t(stride - width);
-        }
-    
-    typename AdaptorType::difference_type distance_to(image_iterator const& i_i) const {
-        ptrdiff_t raw_distance = i_i.base() - this->base() - i_i.crnt_pos + this->crnt_pos;
-        ptrdiff_t count_rows = raw_distance / ptrdiff_t(stride);
-        return i_i.base() - this->base() - count_rows * (stride - width);
-        };
-        
-    bool equal(AdaptorType const& i_i) const{
-        return this->base() == i_i.base();
-        }
-        
-    size_t stride;
-    size_t width;
-    ptrdiff_t crnt_pos = 0;
-};
 
+    void advance(typename image_iterator::difference_type n)
+    {
+        int row = floor(double(index + n) / width);
+        this->base_reference() += row * (stride - width) + n;
+        index += n - row * width;
+    } 
+
+    typename image_iterator::difference_type distance_to(const image_iterator& i) const
+    {
+        return ((i.base_reference() - i.index) - (this->base_reference() - index)) / static_cast<ptrdiff_t>(stride) * width + (i.index - index);
+    }
+};
 //}
 
 #endif // __ITERATOR_HPP__
