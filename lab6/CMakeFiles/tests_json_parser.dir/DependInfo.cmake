de8716a39d8e9c4db2cd4568d826755b15ed328d
NikitaTok <nek.tokar20466@mail.ru>
2024-12-07 08:53:02 +0000

    lab6

---
+++ lab6/CMakeFiles/tests_json_parser.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/master-programming-task/lab6/tests/json.cpp" "/workspaces/master-programming-task/lab6/CMakeFiles/tests_json_parser.dir/tests/json.cpp.o"
+  "/workspaces/master-programming-task/lab6/tests/quoted_string.cpp" "/workspaces/master-programming-task/lab6/CMakeFiles/tests_json_parser.dir/tests/quoted_string.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "_deps/catch2-src/src/catch2/.."
+  "_deps/catch2-build/generated-includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/master-programming-task/lab6/_deps/catch2-build/src/CMakeFiles/Catch2WithMain.dir/DependInfo.cmake"
+  "/workspaces/master-programming-task/lab6/_deps/catch2-build/src/CMakeFiles/Catch2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab6/CMakeFiles/tests_json_parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_json_parser.dir/tests/json.cpp.o"
+  "CMakeFiles/tests_json_parser.dir/tests/quoted_string.cpp.o"
+  "tests_json_parser"
+  "tests_json_parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_json_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
